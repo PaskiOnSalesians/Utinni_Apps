bbd1a468661f2814ce89965bfe4eb895636cb479
nomissolrac <61462036+Ahegaox@users.noreply.github.com>
2022-02-04 18:08:53 +0100

    Welcome_page

---
+++ flutter_login_signup/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'src/welcomePage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return MaterialApp(
+      title: 'DragonApp',
+      theme: ThemeData(
+         primarySwatch: Colors.purple,
+         textTheme:GoogleFonts.latoTextTheme(textTheme).copyWith(
+           bodyText1: GoogleFonts.montserrat(textStyle: textTheme.bodyText1),
+         ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: WelcomePage(),
+    );
+  }
+}
