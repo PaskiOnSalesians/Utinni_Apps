6995810450894e06c9b0b1cce8686b51770f09fa
Gerard <91083698+Raips@users.noreply.github.com>
2022-02-25 18:10:43 +0100

    avances mapa google

--- flutter_login_signup/lib/src/mapgo.dart
+++ flutter_login_signup/lib/src/mapgo.dart
@@ -1,21 +1,46 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Mapgo extends StatefulWidget {
-  @override
-  _MapgoState creatState() => _MapgoState();
+  final LatLng Casa = LatLng(41.39473136159721, 2.1276748636311775);
+  final LatLng Estacio = LatLng(41.39836145166467, 2.1258408980216323);
 
   @override
-  State<StatefulWidget> createState() {
-    throw UnimplementedError();
-  }
+  _MapgoState createState() => _MapgoState();
 }
 
 class _MapgoState extends State<Mapgo> {
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      initialCameraPosition: CameraPosition(target: LatLng(0, 0), zoom: 12),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mapa districte Sarria'),
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(41.39473136159721, 2.1276748636311775),
+          zoom: 19,
+        ),
+        markers: _createMarkers(),
+      ),
     );
   }
+
+  Set<Marker> _createMarkers() {
+    var tmp = Set<Marker>();
+
+    tmp.add(Marker(
+        markerId: MarkerId("Casa"),
+        position: widget.Casa,
+        infoWindow: InfoWindow(title: "Casa Howdgawards")));
+    tmp.add(Marker(
+        markerId: MarkerId("estacio"),
+        position: widget.Estacio,
+        infoWindow: InfoWindow(title: "Estació how")));
+    return tmp;
+  }
 }
+
+class Casa {}
