c8ec008f8b344687616c9befc457fc8bdd3df414
DevPaski <71769140+DevPaski@users.noreply.github.com>
2022-03-10 01:15:02 +0100

    Update porque todo sale mal

---
+++ DragonApp/dragon_app/lib/src/screens/mapgo.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:geolocator/geolocator.dart';
+
+class Mapgo extends StatefulWidget {
+  final LatLng Casa = LatLng(41.39473136159721, 2.1276748636311775);
+  final LatLng Estacio = LatLng(41.39836145166467, 2.1258408980216323);
+
+  @override
+  _MapgoState createState() => _MapgoState();
+}
+
+class _MapgoState extends State<Mapgo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mapa districte Sarria'),
+      ),
+      body: GoogleMap(
+        myLocationButtonEnabled: true,
+        myLocationEnabled: true,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(41.39473136159721, 2.1276748636311775),
+          zoom: 19,
+        ),
+        markers: _createMarkers(),
+      ),
+    );
+  }
+
+  Set<Marker> _createMarkers() {
+    var tmp = Set<Marker>();
+
+    tmp.add(Marker(
+        markerId: MarkerId("Casa"),
+        position: widget.Casa,
+        infoWindow: InfoWindow(title: "Casa How")));
+    tmp.add(Marker(
+        markerId: MarkerId("estacio"),
+        position: widget.Estacio,
+        infoWindow: InfoWindow(title: "Estació how")));
+    return tmp;
+  }
+}
+
+class Casa {}
