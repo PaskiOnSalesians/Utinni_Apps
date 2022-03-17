214ae385edd798558ce5f7132ae28471f36c3537
DevPaski <71769140+DevPaski@users.noreply.github.com>
2022-03-17 04:05:37 +0100

    Creo que lo he sacado falta mirar por el mobil
    Hay que poner los permisos manualmente a la app instalada en el mobil y eran 4 mierdas.
    A ver si revisamos bien las cosas.
    
    Total de horas gastadas by Paski = 8h.

---
+++ paski_maps_made/flutter_google_map/lib/main.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:flutter_google_map/pages/google_map.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyGoogleMap(title: 'Google Maps by DevPaski'),
+    );
+  }
+}
