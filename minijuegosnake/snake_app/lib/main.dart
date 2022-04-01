d13b05366667e0794d997f85c2ad70e1be107370
Gerard <91083698+Raips@users.noreply.github.com>
2022-04-01 16:44:16 +0200

    juego snake
    pequeño minijuego

---
+++ minijuegosnake/snake_app/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'game.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nagini Game',
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Nagini game"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.brown[500],
+      body: Game(),
+    );
+  }
+}
