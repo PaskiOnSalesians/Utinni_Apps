125c81c6a47ca2ad0cb966482bfaef0223830de8
nomissolrac <61462036+Ahegaox@users.noreply.github.com>
2022-04-06 20:41:04 +0200

    Preguntas

--- QuizCarlos_Paski/quiz_horrocrux/lib/models/Questions.dart
+++ QuizCarlos_Paski/quiz_horrocrux/lib/models/Questions.dart
@@ -51,3 +51,88 @@ const List sample_data = [
     "answer_index": 1,
   },
 ];
+
+/*
+HERBOLOGÍA
+{
+  "id": 1,
+  "question": "¿A parte de en Hogwarts, en que otra escuela mágica se sabe que imparten esta asignatura?",
+  "options": ['Castelobruxo', 'Instituto Durmstrang', 'Colegio Ilvermorny de Magia y Hechicería', 'Mahoutokoro'],
+  "answer_index": 0,
+},
+{
+  "id": 2,
+  "question": "¿Quien fue profesor de esta materia después de Herbert Beery?",
+  "options": ['Sybill Trelawney', 'Neville Longbottom', 'Pomona Sprout', 'Beedle el Bardo'],
+  "answer_index": 2,
+},
+{
+  "id": 3,
+  "question": "¿Quien de estos no consiguió el TIMO en herbología?",
+  "options": ['Barty Crouch Jr', 'Seamus Finnigan', '	Bill Weasley', 'Dean Thomas'],
+  "answer_index": 1,
+},
+{
+  "id": 4,
+  "question": "¿Como pueden ser destruidos los pinchos de los arbustos puntiagudos?",
+  "options": ['Incendio', 'Reducto', 'Lumos', 'Impedimenta'],
+  "answer_index": 3,
+},
+{
+  "id": 5,
+  "question": "¿Cuando se da la clase en primer curso?",
+  "options": ['Lunes', 'Miercoles', 'Jueves', 'Viernes'],
+  "answer_index": 1,
+},
+{
+  "id": 6,
+  "question": "Libro Obligatorio",
+  "options": ['Plantas carnivoras del mundo', 'Arboles carnivoros del mundo', 
+              'Plantas y arboles carnivoros del mundo', 'Herbología general'],
+  "answer_index": 1,
+},
+];
+*/
+
+/*
+CALLEJÓN DIAGON
+{
+  "id": 1,
+  "question": "¿Los magos y las brujas dónde sacan el dinero?",
+  "options": ['Gringotts', 'Ollivander’s', 'Flourish & Blotts', 'Gambol y Japes'],
+  "answer_index": 0,
+},
+{
+  "id": 2,
+  "question": "¿Qué son los “knuts”?",
+  "options": ['El nombre del banco de los magos y las brujas', 'Un bar donde Hagrid lleva a Harry',
+             'Una moneda mágica de bronce', 'Uno de los libros que Harry necesita para Hogwarts'],
+  "answer_index": 2,
+},
+{
+  "id": 3,
+  "question": "¿Qué es el “Caldero Chorreante”?",
+  "options": ['El banco de los magos y brujas', 'Un bar pequeño pero muy famoso',
+              'El lugar en el que duermen los Dursley', 'La bebida favorita de magos y brujas'],
+  "answer_index": 1,
+},
+{
+  "id": 4,
+  "question": "¿Qué profesor encuentran Hagrid y Harry en su visita al “Caldero Chorreante”?",
+  "options": ['Profesor Snape', 'Profesor Dumbledore', 'Profesor Slughorn', 'Profesor Quirrel'],
+  "answer_index": 3,
+},
+{
+  "id": 5,
+  "question": "Las varitas son...",
+  "options": ['las que escogen al mago', 'indestructibles', 'para todos iguales', 'lo único necesario para ir a Hogwarts'],
+  "answer_index": 0,
+},
+{
+  "id": 6,
+  "question": "Para ir a Hogwarts, Harry tiene que coger el tren en la estación de...",
+  "options": ['Ollivanders', 'Gringotts', 'Kings Cross', '9 y 3/4'],
+  "answer_index": 2,
+},
+];
+*/
